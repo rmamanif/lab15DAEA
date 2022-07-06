7d3005b08c3bcad1897d0875d434319f9759ab3c
Richard Mamani <richard.mamani@tecsup.edu.pe>
2022-07-06 15:00:15 -0500

    Agregar archivos de proyecto.

---
+++ Infraestructure/ExampleContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Domain;
+
+namespace Infraestructure
+{
+    public class ExampleContext : DbContext
+    {
+
+        public ExampleContext() : base("name = MyContextDB")
+        {
+
+        }
+        public DbSet<Person> People { get; set; }
+    }
+}
